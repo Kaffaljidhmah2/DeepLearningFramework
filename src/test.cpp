cb94e86614e4dd59d002ed22b29e804e8e8b3093
HackyHuang <visualstudio2011@163.com>
2019-03-23 20:25:37 +0800

    test on static graph

--- src/test.cpp
+++ src/test.cpp
@@ -103,25 +103,24 @@ int main()
 	// // Build Graph
 
 
-	Variable x(3),y(2);
-	Variable & w=Graph::Add(x,y);
-	Variable & z=Graph::Add(w,w);
+	// Variable x(3),y(2);
+	// Variable & w=Graph::Add(x,y);
+	// Variable & z=Graph::Add(x,w);
 
-	// Don't write Variable z=Graph.Add(x,y); the move constructor will ruin the variable stored in Graph !
+	// // Don't write Variable z=Graph.Add(x,y); the move constructor will ruin the variable stored in Graph !
 
-	// Graph builds graph first
-	cout<<z<<endl;
+	// // Graph builds graph first
+	// cout<<z<<endl;
 	
-	Graph::eval(z);
-	cout<<z<<endl;
+	// Graph::eval(z);
+	// cout<<z<<endl;
 
-	//Auto_diff
-	Graph::backward(z);
-	cout<<*x.grad<<endl;
-	cout<<*y.grad<<endl;
+	// //Auto_diff
+	// Graph::backward(z);
+	// cout<<*x.grad<<endl;
+	// cout<<*y.grad<<endl;
 
-	// Example 2 Gradient Descend
-	// Graph g;
+	// // Example 2 Gradient Descend
 	// float lr=0.1;
 	// int total=200;
 
@@ -129,28 +128,27 @@ int main()
 	// Variable vx(std::move(x)), vy(std::move(y));
 
 	// Variable M({2,2});*M.data={1,0,2,1};
-	// Variable & mx = g.MatMul(M,vx);
-	// Variable & residual = g.Sub(mx,vy);
-	// Variable & loss = g.InnerProduct(residual,residual);
+	// Variable & mx = Graph::MatMul(M,vx);
+	// Variable & residual = Graph::Sub(mx,vy);
+	// Variable & loss = Graph::InnerProduct(residual,residual);
 
 	// for (int epoch=0;epoch<total;++epoch)
 	// {
-	// 	g.eval(loss);
+	// 	Graph::eval(loss);
 	// 	cout<<loss.data->p[0]<<endl;
-	// 	g.zero_grad();
-	// 	g.backward(loss);
+	// 	Graph::zero_grad();
+	// 	Graph::backward(loss);
 	// 	*vx.data-=functional::cmul(lr,*vx.grad);
 	// 	//lr*=0.9;
 	// }
 
 	// Graph calculates only the expression that leads to it
-
-	// Graph g;
+	
 	// Variable x(2),y(3),z(5);
-	// Variable & w=g.Add(x,y);
-	// Variable & t=g.Sub(y,z);
-	// Variable & t2=g.MatMul(t,x);
-	// g.eval(t2);
+	// Variable & w=Graph::Add(x,y);
+	// Variable & t=Graph::Sub(y,z);
+	// Variable & t2=Graph::MatMul(t,x);
+	// Graph::eval(t2);
 	// cout<<w<<endl;	// w will be empty because it doesn't lead to t2.
 	// cout<<t<<endl;
 	// cout<<t2<<endl;
